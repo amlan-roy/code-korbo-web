35482cfd1f27677c63751f8c961cbf2adbc7a7f8
Amlan Roy <amlanroy2500@gmail.com>
2023-10-31 18:39:08 +0530

    Route guarded home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,40 @@
-import Image from "next/image";
+"use client";
+
+import { auth } from "@/firebase/firebase";
+import { useEffect } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { redirect } from "next/navigation";
+import LoadingPage from "@/components/Pages/LoadingPage";
+import { Typography } from "@mui/material";
+import ErrorPage from "@/components/Pages/ErrorPage";
 
 export default function Home() {
-  return <div className="flex w-full grow flex-col"></div>;
+  const [user, loading, authError] = useAuthState(auth);
+
+  useEffect(() => {
+    if (!loading && !user) redirect("/auth");
+  }, [user, loading]);
+
+  if (loading) {
+    return <LoadingPage />;
+  }
+  if (authError) {
+    return (
+      <ErrorPage
+        title="Oops, an error occurred!"
+        subtitle={authError.message}
+      />
+    );
+  }
+
+  return (
+    <div className="flex w-full grow flex-col">
+      <Typography variant="h3" align="center" mx={2} mt={5}>
+        Practice Kar Le Bhai!!!
+      </Typography>
+      <div className="relative overflow-x-auto mx-auto px-6 pb-10 w-full h-full">
+        {/* Questions table will come here */}
+      </div>
+    </div>
+  );
 }
